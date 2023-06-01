77d908baa65ba55409d985ba30a123e499af0b89
VladislavDeveloper <studentredin323@gmail.com>
2023-06-01 17:23:09 +0300

    project started

--- routes/web.php
+++ routes/web.php
@@ -13,36 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-$news = [
-    [
-        'id' => 1,
-        'title' => 'Получено первое рентгеновское изображение отдельного атома'
-    ],
-    [
-        'id' => 2,
-        'title' => 'BI.ZONE: мошенники похитили более 560 доменов и превратили их в фишинговые сайты'
-    ],
-    [
-        'id' => 3,
-        'title' => 'РЖД представила собственного виртуального помощника «Валера»'
-    ],
-    [
-        'id' => 4,
-        'title' => '«МегаФон» и Банки.ру представят совместный проект'
-    ]
-];
-
-$username = 'Пользователь';
-
-
-Route::get('/', function () use ($username) {
-    return view('home')->with('username', $username);
-});
-
-Route::get('/news', function () use ($news) {
-    return view('news')->with('news', $news);
-});
-
-Route::get('/about', function () {
-    return view('about');
+Route::get('/', function () {
+    return view('welcome');
 });
